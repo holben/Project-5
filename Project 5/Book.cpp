e6275a324ced00cd572f38fbf84a0dab1b526dc5
holben <bihwolf@gmail.com>
2016-04-22 09:58:01 -0600

    fds

---
+++ Project 5/Book.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Book.h"
+
+
+Book::Book()
+{
+}
+
+
+Book::~Book()
+{
+}
---
+++ Project 5/Book.h
@@ -0,0 +1,17 @@
+#pragma once
+class Book
+{
+public:
+	Book();
+	~Book();
+};
+
+class AudioBook : public Book
+{
+
+};
+
+class DigitalBook : public Book
+{
+
+};
